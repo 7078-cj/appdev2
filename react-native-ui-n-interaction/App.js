6ee36e7e974dc0c1b3b5bdc6b59a70ccf7310a24
7078-cj <7078ceejay@gmail.com>
2026-02-13 12:13:22 +0800

    Done with fixed dimension section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import LotsOfStyles from './components/LotsOfStyles';
+import FixedDimensionsBasics from './components/FixedDimensionsBasics';
 
 export default function App() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <LotsOfStyles/>
+      <FixedDimensionsBasics/>
     </ScrollView>
   );
 }
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 40
   },
 });
