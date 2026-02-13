96d900700ea679e8982d4d65c1b093433130cd2a
7078-cj <7078ceejay@gmail.com>
2026-02-13 12:32:44 +0800

    Done with flex direction section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Flex from './components/Flex';
+import FlexDirectionBasics from './components/FlexDirectionBasics';
 
 export default function App() {
   return (
     <>
-      <Flex/>
+      {/* <Flex/> */}
+      <FlexDirectionBasics/>
     </>
   );
 }
