b711703c65ea0977d860260bad9db81389fd99ed
7078-cj <7078ceejay@gmail.com>
2026-02-17 23:30:44 +0800

    Done with flex wrap section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -7,6 +7,7 @@ import JustifyContentBasics from './components/JustifyContentBasics';
 import AlignItemsLayout from './components/AlignItemsLayout';
 import AlignSelfLayout from './components/AlignSelfLayout';
 import AlignContentLayout from './components/AlignContentLayout';
+import FlexWrapLayout from './components/FlexWrapLayout';
 
 export default function App() {
   return (
@@ -17,7 +18,8 @@ export default function App() {
       {/* <JustifyContentBasics/> */}
       {/* <AlignItemsLayout/> */}
       {/* <AlignSelfLayout/> */}
-      <AlignContentLayout/>
+      {/* <AlignContentLayout/> */}
+      <FlexWrapLayout/>
     </>
   );
 }
